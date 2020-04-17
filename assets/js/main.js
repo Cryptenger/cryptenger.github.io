5233a80d06673d23cb34d1358cf9c77609c769c4
Luitot <adrien.luitot@outlook.com>
2020-04-17 19:24:35 +0200

    Added Downloads page
    Added Downloads page and fixed few typos

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,43 @@ $(".navbar-toggler").on('click', function () {
         $("#mainNavbar").animate({opacity: 0}, 400, "linear", function () { $(this).toggleClass("active"); });
     }
 });
+
+$('.dl-selection').on('click', 'button', function (e) {
+    e.stopPropagation();
+    let button = $(this).closest('.selector-container').find('.downloader');
+    let extension = $(this).attr('id').split('-')[1].split('_').join('.');
+
+    let url;
+    if (button.attr('id').split('-')[0] === "linux") {
+        url = button.attr('href').split('/');
+        url[url.length-1] = "cryptenger_client." + extension;
+        url = url.join('/')
+    } else {
+        url = button.attr('href').split('.');
+        if(url[url.length-1] === "gz") url.pop();
+        url[url.length-1] = extension;
+        url = url.join('.');
+    }
+
+    button.attr('href', url);
+
+    button.find('span').text($(this).text());
+    $(this).parent().hide();
+});
+
+$(document).on('click',  function (e) {
+    let parent =  $(e.target);
+
+    if(parent.hasClass('fa-chevron-down')) parent = parent.parent();
+
+    if($(parent).hasClass('dl-selector')) {
+        let id = '#'+parent.attr("id").split("-")[0]+((parent.attr("id").split("-").length===3)? '-source': '')+'-selection';
+        $('.dl-selection:not('+id+')').hide();
+    } else {
+        $('.dl-selection').hide();
+    }
+});
+
+$('.dl-selector').on('click', function () {
+    $(this).parent().find('.dl-selection').show();
+});
