0ac2a0f41e407b75cb3129f27ca300b8bbd1b075
Adrien Luitot <adrien.luitot@outlook.com>
2020-04-22 15:52:47 +0200

    Huge update
    Too much things to be enumerated here. Go to the changelogs of the website, tab "Site", it corresponds to the v1.0.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 $(".navbar-toggler").on('click', function () {
-    $(this).toggleClass("active");
-    
-    if(!$("#mainNavbar").hasClass("active")) {
-        $("#mainNavbar").toggleClass("active").animate({opacity: 1}, 400);
+    if($("#mainNavbar").hasClass("active")) {
+        $(this).removeClass("active");
+        $("#mainNavbar").removeClass("visible");
+        setTimeout(() => { $("#mainNavbar").removeClass("active"); }, 400);
     } else {
-        $("#mainNavbar").animate({opacity: 0}, 400, "linear", function () { $(this).toggleClass("active"); });
+        $(this).addClass("active");
+        $("#mainNavbar").addClass("active");
+        setTimeout(() => { $("#mainNavbar").addClass('visible'); }, 50);
     }
 });
 
